7bcb35f867277aeeb3b59d18abf1d4a39b64247e
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-04-11 22:34:53 -0400

    swagger installed, default settings and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TesteModule } from './teste/teste.module';
 import { TestModule } from './resources/test/test.module';
 import { UsersModule } from './resources/users/users.module';
 import { AuthModule } from './resources/auth/auth.module';
 
 @Module({
-  imports: [TesteModule, TestModule, UsersModule, AuthModule],
+  imports: [TestModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+// nest core
 import { NestFactory } from '@nestjs/core';
+//swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+//resource app module
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  //port
   await app.listen(3000);
 }
 bootstrap();
