e5bc7b6d860e084ba75caabf55da757afdee19b4
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-04-13 16:24:50 -0400

    Swagger apitags created for each resource

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,9 @@ async function bootstrap() {
     .setTitle('Cats example')
     .setDescription('The cats API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('test')
+    .addTag('users')
+    .addTag('auth')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
--- src/resources/test/test.controller.ts
+++ src/resources/test/test.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TestService } from './test.service';
 import { CreateTestDto } from './dto/create-test.dto';
 import { UpdateTestDto } from './dto/update-test.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('test')
 @Controller('test')
 export class TestController {
   constructor(private readonly testService: TestService) {}
