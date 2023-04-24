118c6c88401b2aeebb1acf2d80a0aec83e483c9e
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-04-24 16:44:06 -0400

    Using Prisma Service at resource auth/auth.service.ts

--- src/resources/auth/auth.service.ts
+++ src/resources/auth/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@nestjs/common';
+
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+
+//CRUD
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
+
+  async validateUser(username: string, password: string) {
+    const user = await this.prisma.client.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+  //crud-test
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
