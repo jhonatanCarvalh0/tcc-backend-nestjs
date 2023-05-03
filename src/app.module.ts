d5dc8ba1282482a780bc923a64871c4535f2e53a
Jhonatan Carvalho <jhonatan.silva.4198@gmail.com>
2023-05-03 15:30:53 -0400

    duplicate resolved

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,8 +6,6 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { BensMoveisModule } from './bens-moveis/bens-moveis.module';
-import { BensMoveisModule } from './bens-moveis/bens-moveis.module';
-import { BensMoveisModule } from './bens-moveis/bens-moveis.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), AuthModule, UsersModule, BensMoveisModule],
